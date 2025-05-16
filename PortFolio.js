ce7838352a6e445fe83a7ee867ec3685c2e045e1
D98Coder <riteshku7312@gmail.com>
2025-05-16 21:04:42 +0530

    my portfolio

---
+++ PortFolio.js
@@ -0,0 +1,8 @@
+    
+let typed = new Typed(".text", {
+    strings: ["Fronted Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
